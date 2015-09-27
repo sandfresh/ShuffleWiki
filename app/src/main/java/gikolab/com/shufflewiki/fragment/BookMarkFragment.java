6429fdcd473951ec1013413b71b89fb5f874fc37
sandfresh <sandfresh@gmail.com>
2015-09-28 01:34:13 +0800

    7th

--- app/src/main/java/gikolab/com/shufflewiki/fragment/BookMarkFragment.java
+++ app/src/main/java/gikolab/com/shufflewiki/fragment/BookMarkFragment.java
@@ -1,8 +1,6 @@
 package gikolab.com.shufflewiki.fragment;
 
 import android.app.Fragment;
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -28,171 +26,152 @@ import gikolab.com.shufflewiki.retrive.Wiki;
 /**
  * Created by stalin on 2015/9/26.
  */
-public class BookMarkFragment extends Fragment
-{
-  private ListView mListView;
-  private Handler mUpdateHandler;
-  private List<String> mDataList;
-  private CustomAdapter mListAdapter;
+public class BookMarkFragment extends Fragment {
+    private ListView mListView;
+    private Handler mUpdateHandler;
+    private List<String> mDataList;
+    private CustomAdapter mListAdapter;
 
 
-  private View mView;
+    private View mView;
 
-  @Override
-  public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                           Bundle savedInstanceState) {
-    mView =  inflater.inflate(R.layout.fragment_bookmark,container, false);
-    Log.e("Fragement","CreatView : List");
-    return mView;
-  }
-
-  @Override
-  public void onActivityCreated(Bundle savedInstanceState)
-  {
-    super.onCreate(savedInstanceState);
-    if (savedInstanceState != null)
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState)
     {
-      Log.e("Fragement","SaveInstacne Not Null");
-
-      FragmentManager fragmentManager = getFragmentManager();
-      FragmentTransaction transaction = fragmentManager
-              .beginTransaction();
-      transaction.show(BookMarkFragment.this);
-      transaction.commit();
-    }
-    else
-    {
-      mDataList = new ArrayList<String>();
-      mListView = (ListView) mView.findViewById(R.id.listview2);
-      mListAdapter = new CustomAdapter(this.getActivity(), R.id.listTextView2, mDataList);
-      mListView.setAdapter(mListAdapter);
-
-      mListView.setClickable(true);
-      mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-        @Override
-        public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
-          String object = arg0.getItemAtPosition(position).toString();
-          Intent intent = new Intent(getActivity(), DetailActivity.class);
-          intent.putExtra("name", object);
-          startActivity(intent);
-        }
-      });
-      mUpdateHandler = new Handler() {
-        @Override
-        public void handleMessage(Message msg) {
-          String MsgString = (String) msg.obj;
-          if (MsgString.equals("UpdateListView"))
-            mListAdapter.notifyDataSetChanged();
-            mListAdapter.getCount();
-        }
-      };
-
-      Wiki.getInstance().registerUpdateListener(new Wiki.IupdateListener() {
-        @Override
-        public void onAddData(String value)
-        {
-          String obj = "UpdateListView";
-          mDataList.add(value);
-          Message message = mUpdateHandler.obtainMessage(1, obj);
-          mUpdateHandler.sendMessage(message);
-        }
-
-        @Override
-        public void onRemove(String value)
-        {
-          String obj = "UpdateListView";
-          mDataList.remove(value);
-          Message message = mUpdateHandler.obtainMessage(1, obj);
-          mUpdateHandler.sendMessage(message);
-        }
-      });
-
-    }
-    Log.e("Fragement", "ActivityCreated : BookMark");
-    initialView();
-  }
-
-
-  public void initialView()
-  {
-    List<String> list = Wiki.getInstance().getBookTitle();
-    for(String str:list)
-    {
-      mDataList.add(str);
-    }
-
-    String obj = "UpdateListView";
-    Message message = mUpdateHandler.obtainMessage(1, obj);
-    mUpdateHandler.sendMessage(message);
-  }
-
-  public class CustomAdapter extends ArrayAdapter<String> {
-
-    private List<String> mList;
-    private LayoutInflater mInflater;
-
-    public CustomAdapter(Context context, int resource, List<String> objects) {
-      super(context, resource, objects);
-      mInflater = LayoutInflater.from(context);
-      mList = objects;
+        mView = inflater.inflate(R.layout.fragment_bookmark, container, false);
+        Log.e("Fragement", "CreatView : List");
+        return mView;
     }
 
     @Override
-    public int getCount() {
-      return mList.size();
-    }
-
-    @Override
-    public String getItem(int position) {
-      return mList.get(position);
-    }
-
-    @Override
-    public long getItemId(int position) {
-      return position;
-    }
-
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent)
+    public void onActivityCreated(Bundle savedInstanceState)
     {
-      //return null;
-      View view;
-      ViewHolder holder = null;
-      if (convertView == null)
-      {
-        view = mInflater.inflate(R.layout.listview_bookmark, parent, false);
-        holder = new ViewHolder();
-        holder.listText = (TextView) view.findViewById(R.id.listTextView2);
-        holder.listBtn = (Button) view.findViewById(R.id.listButton2);
-        view.setTag(holder);
-      }
-      else
-      {
-        view = convertView;
-        holder = (ViewHolder) convertView.getTag();
-      }
+        super.onCreate(savedInstanceState);
 
+        mDataList = new ArrayList<String>();
+        mListView = (ListView) mView.findViewById(R.id.listview2);
+        mListAdapter = new CustomAdapter(this.getActivity(), R.id.listTextView2, mDataList);
+        mListView.setAdapter(mListAdapter);
 
-      if (mList != null) {
-        final String listStr = mList.get(position);
-        holder.listText.setText(listStr);
-        holder.listBtn.setOnClickListener(new View.OnClickListener() {
-          @Override
-          public void onClick(View v)
-          {
-            Wiki.getInstance().removeBookMark(listStr);
-          }
+        mListView.setClickable(true);
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
+                String object = arg0.getItemAtPosition(position).toString();
+                Intent intent = new Intent(getActivity(), DetailActivity.class);
+                intent.putExtra("name", object);
+                startActivity(intent);
+            }
         });
-      }
+        mUpdateHandler = new Handler()
+        {
+            @Override
+            public void handleMessage(Message msg) {
+                String MsgString = (String) msg.obj;
+                if (MsgString.equals("UpdateListView"))
+                    mListAdapter.notifyDataSetChanged();
+                mListAdapter.getCount();
+            }
+        };
 
-      return view;
+        Wiki.getInstance().registerUpdateListener(new Wiki.IupdateListener()
+        {
+            @Override
+            public void onAddData(String value) {
+                String obj = "UpdateListView";
+                mDataList.add(value);
+                Message message = mUpdateHandler.obtainMessage(1, obj);
+                mUpdateHandler.sendMessage(message);
+            }
+
+            @Override
+            public void onRemove(String value) {
+                String obj = "UpdateListView";
+                mDataList.remove(value);
+                Message message = mUpdateHandler.obtainMessage(1, obj);
+                mUpdateHandler.sendMessage(message);
+            }
+        });
+        Log.e("Fragement", "ActivityCreated : BookMark");
+        initialView();
     }
 
-    private class ViewHolder {
-      public Button listBtn;
-      public TextView listText;
+
+    public void initialView()
+    {
+        List<String> list = Wiki.getInstance().getBookTitle();
+        for (String str : list) {
+            mDataList.add(str);
+        }
+
+        String obj = "UpdateListView";
+        Message message = mUpdateHandler.obtainMessage(1, obj);
+        mUpdateHandler.sendMessage(message);
     }
 
+    public class CustomAdapter extends ArrayAdapter<String>
+    {
 
-  }
+        private List<String> mList;
+        private LayoutInflater mInflater;
+
+        public CustomAdapter(Context context, int resource, List<String> objects) {
+            super(context, resource, objects);
+            mInflater = LayoutInflater.from(context);
+            mList = objects;
+        }
+
+        @Override
+        public int getCount() {
+            return mList.size();
+        }
+
+        @Override
+        public String getItem(int position) {
+            return mList.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            //return null;
+            View view;
+            ViewHolder holder = null;
+            if (convertView == null) {
+                view = mInflater.inflate(R.layout.listview_bookmark, parent, false);
+                holder = new ViewHolder();
+                holder.listText = (TextView) view.findViewById(R.id.listTextView2);
+                holder.listBtn = (Button) view.findViewById(R.id.listButton2);
+                view.setTag(holder);
+            } else {
+                view = convertView;
+                holder = (ViewHolder) convertView.getTag();
+            }
+
+
+            if (mList != null) {
+                final String listStr = mList.get(position);
+                holder.listText.setText(listStr);
+                holder.listBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Wiki.getInstance().removeBookMark(listStr);
+                    }
+                });
+            }
+
+            return view;
+        }
+
+        private class ViewHolder
+        {
+            public Button listBtn;
+            public TextView listText;
+        }
+    }
 }
--- app/src/main/java/gikolab/com/shufflewiki/fragment/ListFragment.java
+++ app/src/main/java/gikolab/com/shufflewiki/fragment/ListFragment.java
@@ -1,8 +1,6 @@
 package gikolab.com.shufflewiki.fragment;
 
 import android.app.Fragment;
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -33,8 +31,7 @@ import gikolab.com.shufflewiki.retrive.WikiException;
 /**
  * Created by stalin on 2015/9/26.
  */
-public class ListFragment extends Fragment
-{
+public class ListFragment extends Fragment {
     private ListView mListView;
     private Handler mUpdateHandler;
     private ArrayList<String> mDataList;
@@ -45,8 +42,8 @@ public class ListFragment extends Fragment
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        mView =  inflater.inflate(R.layout.fragement_list, container, false);
-        Log.e("Fragement","CreatView : List");
+        mView = inflater.inflate(R.layout.fragement_list, container, false);
+        Log.e("Fragement", "CreatView : List");
         return mView;
     }
 
@@ -54,73 +51,54 @@ public class ListFragment extends Fragment
     public void onActivityCreated(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        if (savedInstanceState != null)
-        {
-            Log.e("Fragement","SaveInstacne Not Null");
 
-            FragmentManager fragmentManager = getFragmentManager();
-            FragmentTransaction transaction = fragmentManager
-                    .beginTransaction();
-            transaction.show(ListFragment.this);
-            transaction.commit();
-        }
-        else
-        {
-            mDataList = new ArrayList<String>();
-            mListView = (ListView) mView.findViewById(R.id.listview1);
-            mListAdapter = new CustomAdapter(this.getActivity(), R.id.listTextView1, mDataList);
-            mListView.setAdapter(mListAdapter);
+        mDataList = new ArrayList<String>();
+        mListView = (ListView) mView.findViewById(R.id.listview1);
+        mListAdapter = new CustomAdapter(this.getActivity(), R.id.listTextView1, mDataList);
+        mListView.setAdapter(mListAdapter);
 
-            final Button update = (Button) mView.findViewById(R.id.updateBtn);
-            update.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v)
-                {
-                    Log.i("Acitiity", "Update");
-                    ListFragment.this.updateListView();
-                }
-            });
+        final Button update = (Button) mView.findViewById(R.id.updateBtn);
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("Acitiity", "Update");
+                ListFragment.this.updateListView();
+            }
+        });
 
-            mListView.setClickable(true);
-            mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        mListView.setClickable(true);
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
-                @Override
-                public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
-                    String object = arg0.getItemAtPosition(position).toString();
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
+                String object = arg0.getItemAtPosition(position).toString();
 
-                    Intent intent = new Intent(getActivity(), DetailActivity.class);
-                    intent.putExtra("name",object);
-                    startActivity(intent);
-                }
-            });
-            mUpdateHandler = new Handler() {
-                @Override
-                public void handleMessage(Message msg) {
-                    String MsgString = (String) msg.obj;
-                    if (MsgString.equals("UpdateListView"))
-                        mListAdapter.notifyDataSetChanged();
-                }
-            };
+                Intent intent = new Intent(getActivity(), DetailActivity.class);
+                intent.putExtra("name", object);
+                startActivity(intent);
+            }
+        });
+        mUpdateHandler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                String MsgString = (String) msg.obj;
+                if (MsgString.equals("UpdateListView"))
+                    mListAdapter.notifyDataSetChanged();
+            }
+        };
 
-            updateListView();
-        }
+        updateListView();
+
         Log.e("Fragement", "ActivityCreated : List");
-
-
     }
 
 
-    public void updateListView()
-    {
-        Wiki.getInstance().getTitleInBackground(new GetNetCallBack()
-        {
+    public void updateListView() {
+        Wiki.getInstance().getTitleInBackground(new GetNetCallBack() {
             @Override
-            public void done(JSONObject object, WikiException e)
-            {
-                try
-                {
-                    if (object != null)
-                    {
+            public void done(JSONObject object, WikiException e) {
+                try {
+                    if (object != null) {
                         Log.i("Activity", object.toString());
                         JSONObject query = object.getJSONObject("query");
                         JSONArray random = query.getJSONArray("random");
@@ -134,8 +112,7 @@ public class ListFragment extends Fragment
                         Message message = mUpdateHandler.obtainMessage(1, obj);
                         mUpdateHandler.sendMessage(message);
                     }
-                }
-                catch (Exception e2) {
+                } catch (Exception e2) {
                     e2.printStackTrace();
                 }
             }
@@ -169,8 +146,7 @@ public class ListFragment extends Fragment
         }
 
         @Override
-        public View getView(int position, View convertView, ViewGroup parent)
-        {
+        public View getView(int position, View convertView, ViewGroup parent) {
 
             //return null;
             View view;
